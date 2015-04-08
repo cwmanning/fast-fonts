c698b7aa64eb11644e22853c3ed948d424effb75
cwmanning <chris.manning@viget.com>
2015-04-07 22:08:21 -0400

    Adds index page to list tests

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <title>fast-fonts demo</title>
+    <meta name="description" content="https://github.com/cwmanning/fast-fonts">
+    <?php include "includes/stylesheet.php"; ?>
+    <style>
+      body {
+        padding: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Font Loading Tests</h1>
+    <ul>
+      <li><a href="/cookie.php">Cookie + FontFaceObserver</a></li>
+      <li><a href="/localstorage.php">localStorage</a></li>
+    </ul>
+    <p>For more information about this site, please see the <a href="https://github.com/cwmanning/fast-fonts">project on GitHub</a>.</p>
+  </body>
+</html>
